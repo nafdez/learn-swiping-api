5e0dde0e82780890603c367e9abee5151b69af48
nafdez <fzp.nacho@gmail.com>
2024-04-01 11:11:00 +0200

    User Service interface

--- model/dto/user/update_request.go
+++ model/dto/user/update_request.go
@@ -1,6 +1,7 @@
 package user
 
 type UpdateRequest struct {
+	Token    string `json:"token"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Email    string `json:"email"`
--- service/user_service.go
+++ service/user_service.go
@@ -1,4 +1,17 @@
 package service
 
+import (
+	"learn-swiping-api/model"
+	"learn-swiping-api/model/dto/user"
+)
+
 type UserService interface {
+	Register(user.RegisterRequest) (*model.User, error)
+	Login(user.LoginRequest) (*model.User, error)
+	Token(user.TokenRequest) (*model.User, error) // Login with token
+	Logout(user.TokenRequest) error
+	Account(user.TokenRequest) (*model.User, error)
+	User(username string) (*user.Public, error)
+	Update(user.UpdateRequest) error
+	Delete(user.TokenRequest) error
 }
