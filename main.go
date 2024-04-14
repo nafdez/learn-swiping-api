a681c55a963c117e7220353a86fddb101172e09d
nafdez <fzp.nacho@gmail.com>
2024-04-14 13:24:28 +0200

    Add router to main and init user controller service and repository to test

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"learn-swiping-api/controller"
 	"learn-swiping-api/database"
+	"learn-swiping-api/repository"
+	"learn-swiping-api/service"
 	"log"
+	"net/http"
 
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -13,9 +19,51 @@ func main() {
 		log.Fatalln("Error loading .env file")
 	}
 
-	_, err = database.Connect()
+	db, err := database.Connect()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	// Init
+	userRepo := repository.NewUserRepository(db)
+	userSrvc := service.NewUserService(userRepo)
+	userCtrl := controller.NewUserController(userSrvc)
+
+	// ROUTER
+	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = append(config.AllowMethods, "OPTIONS")
+
+	router.Use(cors.New(config))
+
+	router.GET("/ping", ping)
+
+	authGroup := router.Group("/auth")
+	{
+		authGroup.POST("register", userCtrl.Register)
+		authGroup.POST("login", userCtrl.Login)
+		authGroup.POST("logout", userCtrl.Logout)
+	}
+
+	accountGroup := router.Group("account")
+	{
+		accountGroup.POST("", userCtrl.Account)
+		accountGroup.PUT("", userCtrl.Update)
+		accountGroup.DELETE("", userCtrl.Delete)
+	}
+
+	userGroup := router.Group("users")
+	{
+		userGroup.GET(":username", userCtrl.User)
+
+	}
+
+	router.Run(":8080")
+
+}
+
+func ping(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"message": "pong"})
 }
