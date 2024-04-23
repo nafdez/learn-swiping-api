0034d2141848b45a6790d011149f39c7c2144ced
nafdez <fzp.nacho@gmail.com>
2024-04-23 19:01:57 +0200

    Add deck_controller create and read functions

--- controller/deck_controller.go
+++ controller/deck_controller.go
@@ -1,18 +1,23 @@
 package controller
 
 import (
+	"errors"
+	"learn-swiping-api/erro"
+	"learn-swiping-api/model/dto/deck"
 	"learn-swiping-api/service"
+	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type DeckController interface {
-	Create(*gin.Context)     // POST
-	Deck(*gin.Context)       // GET
-	OwnedDecks(*gin.Context) // GET
-	Decks(*gin.Context)      // GET (from other users)
-	Update(*gin.Context)     // PUT
-	Delete(*gin.Context)     // DELETE
+	Create(*gin.Context)       // POST
+	Deck(*gin.Context)         // GET
+	OwnedDecks(*gin.Context)   // POST (to accept token)
+	Suscriptions(*gin.Context) // POST (from other users)
+	Update(*gin.Context)       // PUT
+	Delete(*gin.Context)       // DELETE
 }
 
 type DeckControllerImpl struct {
@@ -24,15 +29,100 @@ func NewDeckController(service service.DeckService) DeckController {
 }
 
 func (c *DeckControllerImpl) Create(ctx *gin.Context) {
+	var request deck.CreateRequest
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": erro.ErrBadField})
+		return
+	}
+
+	_, err := c.service.Create(request)
+	if err != nil {
+		if errors.Is(err, erro.ErrUserNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{})
 }
 
 func (c *DeckControllerImpl) Deck(ctx *gin.Context) {
+	idParam := ctx.Param("id")
+
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": erro.ErrBadField})
+		return
+	}
+
+	var request deck.ReadOneRequest
+	_ = ctx.ShouldBindJSON(&request) // Not checking on errors since this field is optional
+	request.Id = int64(id)
+
+	deck, err := c.service.Deck(request)
+	if err != nil {
+		if errors.Is(err, erro.ErrBadField) {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if errors.Is(err, erro.ErrDeckNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, deck)
 }
 
 func (c *DeckControllerImpl) OwnedDecks(ctx *gin.Context) {
+	var request deck.ReadOwnedRequest
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	decks, err := c.service.OwnedDecks(request)
+	if err != nil {
+		if errors.Is(err, erro.ErrBadField) {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if errors.Is(err, erro.ErrDeckNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, decks)
 }
 
-func (c *DeckControllerImpl) Decks(ctx *gin.Context) {
+func (c *DeckControllerImpl) Suscriptions(ctx *gin.Context) {
+	var request deck.ReadRequest
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	decks, err := c.service.Suscriptions(request)
+	if err != nil {
+		if errors.Is(err, erro.ErrBadField) {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if errors.Is(err, erro.ErrDeckNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": erro.ErrNotSuscribed})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, decks)
 }
 
 func (c *DeckControllerImpl) Update(ctx *gin.Context) {
